dee4e1ab0905d26546fca5cb3d286c3efdb61b5e
Jan61234 <jananipriya6123@gmail.com>
2025-10-02 16:51:40 +0530

    Added main.py app menu to run all three projects

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ print("1. Chatbot")
 print("2. Tic-Tac-Toe AI")
 print("3. Recommendation System")
 print("4. Exit")
+while True:
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == "1":
+        import Task1_Chatbot.chatbot
+    elif choice == "2":
+        import Task2_TicTacToe.tictactoe
+    elif choice == "3":
+        import Task3_RecommendationSystem.recommendation
+    elif choice == "4":
+        print("Exiting. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter 1, 2, 3, or 4.")
